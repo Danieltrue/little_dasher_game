bcbc221750a8fe149783a4cb41e3febc1a4bcb02
DanielTrue <danielokezie75@gmail.com>
2021-12-07 09:41:27 +0100

    added player feat e.g jumping, acceleration and frame control

--- dasher.cpp
+++ dasher.cpp
@@ -25,25 +25,62 @@ int main()
 
   //World Properity--------------------------------------------------------------
   int velocity{0};
-  const int jumpValue{10};
-  //World Properity--------------------------------------------------------------
+  const int gravity{1'000};
+  const int jumpValue{-600};
+  bool isInAir{false};
+  int frame{};
+  const float updateTime{1.0 / 12.0};
+  float runningTime{};
+  //World Properity----------------------------------------------------------B----
+
+  SetTargetFPS(60);
+
 
   while(!WindowShouldClose()) {
+
+    const float dT{GetFrameTime()};
+
     BeginDrawing();
 
     ClearBackground(WHITE);
     
-    scarfyPos.y += velocity;
+    if(scarfyPos.y >= windowHeight - scarfyRec.height) {
+      velocity = 0;
+      isInAir = false;
+    } else {
+      //updating the velocity to the gravity for the PLAYER to go down
+      velocity += gravity * dT;
+      isInAir = true;
+    }
+
+    //creating a Delta Time that capture the FPS and use it to give players more frame in the game
+    
 
     //Add Velocity on SPACE pressed
-    if(IsKeyPressed(KEY_SPACE)) {
-       velocity -= jumpValue;
+    if(IsKeyPressed(KEY_SPACE) && !isInAir) {
+       velocity += jumpValue;
     }
+
+    //Updating the velocity
+    scarfyPos.y += velocity * dT;
+    runningTime += dT;
+    if(runningTime >= updateTime) {
+      runningTime = 0.0;
+      //updating the frame
+      scarfyRec.x = frame * scarfyRec.width;
+      frame++;
+      if(frame > 5) {
+        frame = 0;
+      }
+    }
+    
+    
 
     //Draw the character on the screen
     DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
     EndDrawing();
   }
+  UnloadTexture(scarfy);
   CloseWindow();
 } 
